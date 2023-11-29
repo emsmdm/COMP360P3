de42fe61b77f6395325821e3e6b265d0eeb6f58a
emsmith2000 <145351577+emsmdm@users.noreply.github.com>
2023-11-29 18:44:42 -0500

    purchaseTicket and toString

--- Plane.java
+++ Plane.java
@@ -11,12 +11,31 @@ public class Plane {
     private int planeTime;
     private int planeDate;
     private int planeSeats;
+    private int seatNumber;
 
     public Plane(int date, int time)
     {
         planeDate = date;
         planeTime = time;
         planeSeats = 20;
+        seatNumber = 0;
+    }
+
+    public purchaseTicket(int date, int time, Plane planes[]){
+        for(int i = 0; i < planes.length; i++){
+            if(planes[i].planeDate == date && planes[i].planeTime == time){
+                if(planes[i].planeSeats > 0){
+                    planes[i].planeSeats -= 1;
+                    planes[i].seatNumber += 1;
+                    toString(date, time, planes[i]);
+                }
+            }
+        }
+    }
+
+    public String toString(int date, int time, Plane planes[]){
+        String ticketDetails = String.format("Departure date: %d\nDeparture time: %d\nSeat number: %d", date, time, seatNumber);
+        return ticketDetails;
     }
 
     void returnTicket(int date, int time, Plane planes[])
